424082397e17eb2dc01646b80c6902b008f680f7
geoabuladze01rus-eng <geo.abuladze01rus@gmail.com>
2026-08-21 19:37:43 +0300

    test(core): verify priority event ordering

---
+++ tests/test_event_bus.py
@@ -0,0 +1,14 @@
+from core.event_bus import EventBus, EventPriority
+
+def test_critical_event_precedes_normal():
+    bus = EventBus()
+    bus.publish('ticker', {}, EventPriority.NORMAL)
+    bus.publish('exchange_outage', {}, EventPriority.CRITICAL)
+    assert bus.next().event_type == 'exchange_outage'
+
+def test_same_priority_preserves_order():
+    bus = EventBus()
+    bus.publish('a', {}, EventPriority.HIGH)
+    bus.publish('b', {}, EventPriority.HIGH)
+    assert bus.next().event_type == 'a'
+    assert bus.next().event_type == 'b'
